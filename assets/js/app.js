d332b7c1403148d53fe47591ef58762d0c29cf2f
C.Pouil <c.pouil@cir.fr>
2019-03-26 15:12:36 +0100

    Nettoyage app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,20 @@
 "use strict";
 
-// window.onload = function () {
-    function letsLeet() {
-        console.log("----> Let's leet dear...");
+function letsLeet() {
+    console.log("----> Let's leet dear...");
 
-        // get user infos
-        let firstName = prompt('Veuillez saisir votre prénom');
-        let lastName = prompt('Veuillez saisir votre nom');
-        let town = prompt('Veuillez saisir votre ville');
-        let job = prompt('Veuillez saisir votre job');
-        
-        // transform infos into leet speak &
-        // fill html id spans with the formatted infos
-        document.getElementById('fname').innerText = convertLeet(firstName);
-        document.getElementById('lname').innerText = convertLeet(lastName);
-        document.getElementById('town').innerText = convertLeet(town);
-        document.getElementById('job').innerText = convertLeet(job);
+    // get user infos
+    let firstName = prompt('Veuillez saisir votre prénom');
+    let lastName = prompt('Veuillez saisir votre nom');
+    let town = prompt('Veuillez saisir votre ville');
+    let job = prompt('Veuillez saisir votre job');
+    
+    // transform infos into leet speak &
+    // fill html id spans with the formatted infos
+    document.getElementById('fname').innerText = convertLeet(firstName);
+    document.getElementById('lname').innerText = convertLeet(lastName);
+    document.getElementById('town').innerText = convertLeet(town);
+    document.getElementById('job').innerText = convertLeet(job);
 
-        console.log("----> Conversion done, you're now a thug big boy.");
-    }
-// }
+    console.log("----> Conversion done, you're now a thug big boy.");
+}
